148ec1383ab4d930c96b2d93e3a75241104c011c
imjord <dean.jay.goat@gmail.com>
2022-03-23 19:20:37 -0600

    have wokring routes to generes but no post yet

--- controllers/fighting.js
+++ controllers/fighting.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 
-const getGenre =  (req,res) => {
-    
 
+
+const getFighting = (req,res) => {
     const response =  fetch("https://api.igdb.com/v4/genres", {
         method: "POST",
         headers: {
@@ -14,13 +14,13 @@ const getGenre =  (req,res) => {
     }).then(response => response.json())
     .then(data => {
       console.log(data)
-      res.render('catagories', {title: 'catagories', category: data})
+      res.render('fighting', {title: 'fighting', games: data})
     })
     .catch(err => console.log(err.message))
 
-   
+
 }
 
 
-// once click on a catagory make it go to a new route that posts that category games
-module.exports = {getGenre}
+
+module.exports = {getFighting}
--- routes/catagoryRoute.js
+++ routes/catagoryRoute.js
@@ -5,6 +5,16 @@ const {
 
 
 router.get('/', getGenre);
+router.get('/fighting', {});
+router.get('/shooter', {});
+router.get('/platform', {});
+router.get('/puzzle', {});
+router.get('/racing', {});
+router.get('/role-playing-rpg', {});
+router.get('/simulator', {});
+router.get('/sport', {});
+
+
 
 
 
